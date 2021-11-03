c8cf7d3163c51f30c64468494425e2743c1e39b0
zaticpetru <zatic.petru@outlook.com>
2021-11-03 16:05:32 +0200

    FE create react app

---
+++ fe-quiz-app/src/components/Button.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './Button.css';
+
+class Button extends Component {
+    render() {
+        return <div className="button" />
+    }
+}
+
+export default Button;
